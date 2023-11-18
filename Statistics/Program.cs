f3f6910c5b930c7d10acc08ff560a860d7633b55
Éderson Nascimento <ederson.nascimento@gmail.com>
2023-11-18 09:24:17 -0300

    Changing remote repository name and adding ignore items

--- Statistics/Program.cs
+++ Statistics/Program.cs
@@ -1,11 +1,15 @@
 ﻿using Statistics.classes;
 
+const int LENGTH = 4;
+const int TOP = 30;
+
 var arguments = Environment.GetCommandLineArgs();
 string fileName = arguments == null || arguments.Length < 2 ? "./resources/combinations-15-big-collection.csv" : arguments[1];
 
+
 if (File.Exists(fileName)) {
     Dictionary<long, int> pairs = new Dictionary<long, int>();
-    var collection = Transform.CreateBitmapRange(1, 11, 25);
+    var collection = Transform.CreateBitmapRange(1, LENGTH, 25);
 
     var lines = File.ReadAllLines(fileName);
     var csv = new ParseCsv(lines);
@@ -31,11 +35,22 @@ if (File.Exists(fileName)) {
     
     pairs = pairs.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
-    int top = (pairs.Count() > 5) ? 5 : pairs.Count();
+    Console.WriteLine($"Total lines {lines.Length}");
+    Console.WriteLine("".PadRight(60, '-'));
+
+    int top = (pairs.Count() > TOP) ? TOP : pairs.Count();
+    long sum = 0;
     for(int i = 0; i < top; i++) {
         var pair = pairs.ElementAt(i);
-        Console.WriteLine($"{i} - Combination {Transform.GetSequence(pair.Key, 25)} - Occours {pair.Value} times");
+        sum |= pair.Key;
+        Console.WriteLine($"{i} - Combination {Transform.GetSequence(pair.Key, 25)} - Occours {pair.Value} times ({Math.Round(((decimal)pair.Value / (decimal)lines.Length) * 100, 2)}%)");
     }
+    
+    var scomb = Transform.GetSequence(sum, 25);
+    var count = scomb.Split(';').Length;
+
+    Console.WriteLine("".PadRight(60, '-'));
+    Console.WriteLine($"Summary {scomb} => {count} numbers");
 } else {
     Console.WriteLine("File not found");
     return;
